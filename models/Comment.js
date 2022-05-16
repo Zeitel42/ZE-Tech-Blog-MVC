b52295e8bbe3d79adcafe10eda48d52a9bb664f7
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-05-16 17:36:44 -0400

    added Comment model

---
+++ models/Comment.js
@@ -0,0 +1,49 @@
+// import important parts of sequelize library
+const { Model, DataTypes, DATE, INTEGER } = require("sequelize");
+// import our database connection from config.js
+const sequelize = require("../config/connection");
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Comment extends Model {}
+
+// set up fields and rules for Product model
+Comment.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "User",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Comment",
+  }
+);
+
+module.exports = Comment;
