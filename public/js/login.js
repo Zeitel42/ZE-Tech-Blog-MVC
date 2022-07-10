c577d4b34e0929c0ee03bb0378356d7b35d589c5
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-07-10 09:18:44 -0400

    added signup.js for separation

--- public/js/login.js
+++ public/js/login.js
@@ -22,41 +22,9 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const username = document
-    .querySelector("#username-input-signup")
-    .value.trim();
-  const password = document
-    .querySelector("#password-input-signup")
-    .value.trim();
-
-  if (username && password) {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({ username, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      document.location.replace("/dashboard");
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
 const loginForm = document.querySelector("#login-form");
 if (loginForm) {
   loginForm.addEventListener("submit", loginFormHandler);
 } else {
   console.log("Your login form is shit");
-}
-
-const signupForm = document.querySelector("#signup-form");
-if (signupForm) {
-  signupForm.addEventListener("submit", signupFormHandler);
-} else {
-  console.log("Your signup form is shit");
 }
